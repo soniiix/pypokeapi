845762116a7f36056d175e0ec663b219e4cf9eac
soniiix <quent.couzinet@gmail.com>
2025-02-02 14:03:08 +0100

    tests: complete image fetch test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,5 @@
 from pypokeapi.pokemons import Pokemon
+from pypokeapi.request import Requester
 import httpx
 
 def test_fetch_ditto():
@@ -30,8 +31,8 @@ def test_init_pokemon():
     assert pokemon.height == 3
     assert pokemon.weight == 40
     assert pokemon.order == 200
-    assert pokemon.image[0].name == "back_default"
-    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/384.png"
+    assert pokemon.image[0].name == "front_default"
+    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/384.png"
 
 def test_from_name():
     pokemon = Pokemon.from_name("ditto")
@@ -41,8 +42,8 @@ def test_from_name():
     assert pokemon.height == 3
     assert pokemon.weight == 40
     assert pokemon.order == 214
-    assert pokemon.image[0].name == "back_default"
-    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
+    assert pokemon.image[0].name == "front_default"
+    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
 
 def test_from_id():
     pokemon = Pokemon.from_id(132)
@@ -52,5 +53,29 @@ def test_from_id():
     assert pokemon.height == 3
     assert pokemon.weight == 40
     assert pokemon.order == 214
-    assert pokemon.image[0].name == "back_default"
-    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
+    assert pokemon.image[0].name == "front_default"
+    assert pokemon.image[0].url == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+
+def test_get_moves():
+    pokemon = Pokemon.from_name("ditto")
+
+    assert pokemon.get("moves")[0].get("move").get("name") == "transform"
+    assert pokemon.get("moves")[0].get("move").get("url") == "https://pokeapi.co/api/v2/move/144/"
+
+def test_fetch_image():
+    pokemon = Pokemon.from_name("pikachu")
+    images = pokemon.image
+
+    assert images[0].name == "front_default"
+    assert images[1].name == "back_default"
+    assert images[2].name == "front_female"
+    assert images[3].name == "back_female"
+    assert images[4].name == "front_shiny"
+    assert images[5].name == "back_shiny"
+    assert images[6].name == "front_shiny_female"
+    assert images[7].name == "back_shiny_female"
+
+def test_random_requester():
+    RandomRequester = Requester()
+
+    
